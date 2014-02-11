d61563d64952702a75dae1805a8f5c3c5d9669d5
Jon Bardin <pair+jon@mavenlink.com>
2014-02-10 20:43:19 -0800

    fix formatting

--- app/assets/javascripts/webrtc.js
+++ app/assets/javascripts/webrtc.js
@@ -1,79 +1,75 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-//console.log(navigator.userAgent, "wtf");
-//var a = navigator.userAgent.match(/WebKit\/([0-9]+)\./)[1]
-//console.log(a);
+  var channels = {};
+  var currentUserUUID = Math.round(Math.random() * 60535) + 5000;
+  var eventSource = null;
 
-    var channels = {};
-    var currentUserUUID = Math.round(Math.random() * 60535) + 5000;
-    var eventSource = null;
+  var connection = new RTCMultiConnection();
 
-    var connection = new RTCMultiConnection();
+  var iceServers = [];
+  iceServers.push({
+    url: "stun:stun.l.google.com:19302"
+  });
 
-    var iceServers = [];
-    iceServers.push({
-        url: "stun:stun.l.google.com:19302"
-    });
+  connection.iceServers = iceServers;
+  connection.sdpConstraints.mandatory = {};
 
-    connection.iceServers = iceServers;
-    connection.sdpConstraints.mandatory = {};
+  connection.openSignalingChannel = function (config) {
 
-    connection.openSignalingChannel = function (config) {
+    if (eventSource === null) {
+      eventSource = new EventSource("primary");
 
-      if (eventSource === null) {
-        eventSource = new EventSource("primary");
+      eventSource.onmessage =  function(e) {
+        console.log(e);
 
-        eventSource.onmessage =  function(e) {
-          console.log(e);
+        data = JSON.parse(e.data);
 
-          data = JSON.parse(e.data);
-
-          if(data.sender == currentUserUUID) {
-            console.log("is mee");
-            return;
-          }
-
-          if (channels[data.channel] && channels[data.channel].onmessage) {
-            channels[data.channel].onmessage(data.message);
-          };
-        };
-      }
-
-        var channel = config.channel || this.channel;
-        channels[channel] = config;
-
-        if (config.onopen) {
-            setTimeout(config.onopen, 1);
+        if(data.sender == currentUserUUID) {
+          console.log("is mee");
+          return;
         }
 
-        return {
-            send: function (message) {
-                request = new XMLHttpRequest();
-                request.open('POST', '/store', true);
-                request.setRequestHeader('Content-Type', 'application/json');
-
-                var wrappedMessage = {
-                    sender: currentUserUUID,
-                    channel: channel,
-                    message: message
-                };
-
-                request.send(JSON.stringify(wrappedMessage));
-            },
-            channel: channel
+        if (channels[data.channel] && channels[data.channel].onmessage) {
+          channels[data.channel].onmessage(data.message);
         };
-    };
+      };
+    }
 
-    connection.onNewSession = function(session) {
-        console.log("!!!!!!", session);
-        connection.join(session);
-    };
+    var channel = config.channel || this.channel;
+    channels[channel] = config;
 
-    document.getElementById("join-room").onclick = function() {
-        connection.connect("CHEESE");
-    };
+    if (config.onopen) {
+      setTimeout(config.onopen, 1);
+    }
 
-    document.getElementById("create-room").onclick = function() {
-        connection.open("CHEESE");
+    return {
+      send: function (message) {
+        request = new XMLHttpRequest();
+        request.open('POST', '/store', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+
+        var wrappedMessage = {
+          sender: currentUserUUID,
+          channel: channel,
+          message: message
+        };
+
+        request.send(JSON.stringify(wrappedMessage));
+      },
+      channel: channel
     };
+  };
+
+  connection.onNewSession = function(session) {
+    console.log("!!!!!!", session);
+    connection.join(session);
+  };
+
+  document.getElementById("join-room").onclick = function() {
+    connection.connect("CHEESE");
+  };
+
+  document.getElementById("create-room").onclick = function() {
+    connection.open("CHEESE");
+  };
 });
